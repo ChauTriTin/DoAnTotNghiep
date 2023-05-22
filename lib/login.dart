66ed46348c00d222ef1fbd0ebe2d0def622d812c
ChauTriTin <113008738+ChauTriTin@users.noreply.github.com>
2023-05-22 22:30:53 +0700

    [Tin] up code

---
+++ lib/login.dart
@@ -0,0 +1,23 @@
+import 'package:appdiphuot/screen/home1_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class Services {
+  final auth = FirebaseAuth.instance;
+  loginUser(email, password, context) async {
+    late String txtEmail;
+    try {
+      await auth.signInWithEmailAndPassword(email: email, password: password);
+      auth.authStateChanges().listen((event) {
+        if (event != null) {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => home1Screen()));
+        }
+      });
+    } catch (e) {
+      final snackBar = SnackBar(content: Text('Lỗi kết nối đến Server'));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }
+  }
+}
