8f2d0640d92a13f21149c8b97a70c3579875e51f
ChauTriTin <freuss47@gmail.com>
2023-05-27 14:55:59 +0700

    update screen create router

--- lib/common/const/color_constants.dart
+++ lib/common/const/color_constants.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 
 class ColorConstants {
   static const Color appColor = Colors.redAccent;
-  static Color appColorBkg = const Color(0xFFE3F2FD);
+  static Color appColorBkg = const Color(0xFFE8E3E3);
   static Color disabledColor = Colors.grey;
 }
