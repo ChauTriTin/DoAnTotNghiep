95444509b97aed11eff38d513c413839dd51cd82
ChauTriTin <freuss47@gmail.com>
2023-05-28 14:59:00 +0700

    update map screen

--- lib/ui/home/router/map/map_screen.dart
+++ lib/ui/home/router/map/map_screen.dart
@@ -5,6 +5,7 @@ import 'package:appdiphuot/common/const/string_constants.dart';
 import 'package:appdiphuot/ui/home/router/map/map_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({
@@ -17,11 +18,15 @@ class MapScreen extends StatefulWidget {
 
 class _MapScreenState extends BaseStatefulState<MapScreen> {
   final _controller = Get.put(MapController());
+  final LatLng _kMapCenter = LatLng(52.4478, -3.5402);
+  GoogleMapController? controller;
+  BitmapDescriptor? _markerIcon;
 
   @override
   void initState() {
     super.initState();
     _setupListen();
+    _createMarkerImageFromAsset(context);
   }
 
   void _setupListen() {
@@ -69,6 +74,52 @@ class _MapScreenState extends BaseStatefulState<MapScreen> {
   }
 
   Widget _buildMapView() {
-    return Container();
+    return Container(
+      child: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: _kMapCenter,
+          zoom: 7.0,
+        ),
+        markers: <Marker>{_createMarker()},
+        onMapCreated: _onMapCreated,
+      ),
+    );
+  }
+
+  Marker _createMarker() {
+    if (_markerIcon != null) {
+      return Marker(
+        markerId: const MarkerId('marker_1'),
+        position: _kMapCenter,
+        icon: _markerIcon!,
+      );
+    } else {
+      return Marker(
+        markerId: MarkerId('marker_1'),
+        position: _kMapCenter,
+      );
+    }
+  }
+
+  Future<void> _createMarkerImageFromAsset(BuildContext context) async {
+    if (_markerIcon == null) {
+      final ImageConfiguration imageConfiguration =
+          createLocalImageConfiguration(context, size: const Size.square(48));
+      BitmapDescriptor.fromAssetImage(
+              imageConfiguration, 'assets/red_square.png')
+          .then(_updateBitmap);
+    }
+  }
+
+  void _updateBitmap(BitmapDescriptor bitmap) {
+    setState(() {
+      _markerIcon = bitmap;
+    });
+  }
+
+  void _onMapCreated(GoogleMapController controllerParam) {
+    setState(() {
+      controller = controllerParam;
+    });
   }
 }
