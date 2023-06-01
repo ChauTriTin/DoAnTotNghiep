2e78610012475fda4d76d5ab00c5d91e616a1144
HoangGiang09012002 <0306201323@caothang.edu.vn>
2023-06-01 21:53:00 +0700

    Save user info to data store

---
+++ lib/model/user.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserData _$UserDataFromJson(Map<String, dynamic> json) => UserData(
+      json['name'] as String,
+      json['uid'] as String,
+      json['email'] as String,
+      json['password'] as String,
+      json['avatar'] as String,
+    );
+
+Map<String, dynamic> _$UserDataToJson(UserData instance) => <String, dynamic>{
+      'name': instance.name,
+      'uid': instance.uid,
+      'email': instance.email,
+      'password': instance.password,
+      'avatar': instance.avatar,
+    };
