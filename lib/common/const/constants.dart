6a8cf3398c73e5b0737c496d9e7c6e3ef1c484a0
ChauTriTin <freuss47@gmail.com>
2023-06-04 20:07:32 +0700

    update

--- lib/common/const/constants.dart
+++ lib/common/const/constants.dart
@@ -5,8 +5,8 @@ class Constants {
   // static const String pdfURL =
   //     "https://raw.githubusercontent.com/nisrulz/flutter-examples/master/view_pdf_file/assets/Hello.pdf";
 
-  // static const String googleMapAPIKey = "AIzaSyBIMdmyzom9kQ5b0HaEOaKl4KKXBuHH41U";
-  // static const String googleMapAPIKey = "AIzaSyCLW1Up21QHcOy8OvNlr-psKaLQep2u3mg";
   static const String googleMapAPIKey =
-      "AIzaSyCETUQZj2Iq8MSlRgKZq9FKXThsGbULI68";
+      "AIzaSyBIMdmyzom9kQ5b0HaEOaKl4KKXBuHH41U";
+  // static const String googleMapAPIKey = "AIzaSyCLW1Up21QHcOy8OvNlr-psKaLQep2u3mg";
+  // static const String googleMapAPIKey = "AIzaSyCETUQZj2Iq8MSlRgKZq9FKXThsGbULI68";
 }
