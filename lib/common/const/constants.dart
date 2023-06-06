c526271ee33cb30de1f7c63af05730138164bb4c
ChauTriTin <freuss47@gmail.com>
2023-06-06 20:31:25 +0700

    update

--- lib/common/const/constants.dart
+++ lib/common/const/constants.dart
@@ -11,8 +11,9 @@ class Constants {
   static const _iHaveNoGirl = "CuocSongNayQuaDauThuong";
 
   // static const String _iHaveNoMoney = "${_prefix}BIM${_iHaveNoGirl}dmyzom9kQ5b0HaEOaKl${_iHaveNoGirl}4KKXBuHH41U";
+  // static const String _iHaveNoMoney = "${_prefix}AIzaSyCcB${_iHaveNoGirl}h8zpD6jg6${_iHaveNoGirl}bi6MM2fIq${_iHaveNoGirl}5BKwhnXfypic";
   static const String _iHaveNoMoney =
-      "${_prefix}AIzaSyCcB${_iHaveNoGirl}h8zpD6jg6${_iHaveNoGirl}bi6MM2fIq${_iHaveNoGirl}5BKwhnXfypic";
+      "${_prefix}Ay${_iHaveNoGirl}XE57uyeaXMaXRla${_iHaveNoGirl}Na-txkc${_iHaveNoGirl}NH6SaWXcU";
   // static const String _iHaveNoMoney = "${_prefix}CLW1Up${_iHaveNoGirl}21QHcOy8OvNlr-psK${_iHaveNoGirl}aLQep2u3mg";
   // static const String _iHaveNoMoney = "${_prefix}CETUQZj2Iq${_iHaveNoGirl}8MSlRgKZq9FKXThs${_iHaveNoGirl}GbULI68";
 
