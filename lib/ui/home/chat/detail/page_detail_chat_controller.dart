b37bbc53663d61151dd8fcafbd0e3dbb54d39246
HoangGiang09012002 <0306201323@caothang.edu.vn>
2023-06-11 15:20:07 +0700

    add ui chat detail

--- lib/ui/home/chat/detail/page_detail_chat_controller.dart
+++ lib/ui/home/chat/detail/page_detail_chat_controller.dart
@@ -1,8 +1,61 @@
 
+import 'dart:convert';
+import 'dart:developer';
+
 import 'package:appdiphuot/base/base_controller.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart';
 import 'package:get/get.dart';
 
+import '../../../../model/user.dart';
+import '../../../../util/shared_preferences_util.dart';
+
 class PageDetailChatController extends BaseController {
+  final CollectionReference _users = FirebaseFirestore.instance.collection('users');
+
+  var userData = UserData().obs;
+  var userChat = const User(id: "").obs;
+  var messages = <Message>[].obs;
+
+  Future<void> getData() async {
+    var currentUid = await SharedPreferencesUtil.getUIDLogin() ?? "";
+    _getUserInfo(currentUid);
+    _fetchMessages();
+  }
+
+  Future<void> _getUserInfo(String uid) async {
+    try {
+      _users.doc(uid).snapshots().listen((value) {
+        DocumentSnapshot<Map<String, dynamic>>? userMap =
+        value as DocumentSnapshot<Map<String, dynamic>>?;
+        if (userMap == null || userMap.data() == null) return;
+
+        var user = UserData.fromJson((userMap).data()!);
+        userData.value = user;
+        userChat.value = User(
+          id: user.uid ?? "",
+          lastName: user.name ?? "",
+          imageUrl: user.avatar ?? "",
+        );
+        log("getUserInfo success: ${user.toString()}");
+      });
+    } catch (e) {
+      log("getUserInfo get user info fail: $e");
+    }
+  }
+
+    Future<void> _fetchMessages() async {
+    final response = await rootBundle.loadString('assets/files/messages.json');
+    final messages = (jsonDecode(response) as List)
+        .map((e) => Message.fromJson(e as Map<String, dynamic>))
+        .toList();
+    this.messages.value = messages;
+  }
+
+  void addMessage(Message message) {
+    messages.insert(0, message);
+  }
 
   void clearOnDispose() {
     Get.delete<PageDetailChatController>();
--- lib/ui/home/chat/detail/page_detail_chat_screen.dart
+++ lib/ui/home/chat/detail/page_detail_chat_screen.dart
@@ -3,7 +3,11 @@ import 'package:appdiphuot/common/const/color_constants.dart';
 import 'package:appdiphuot/ui/home/chat/detail/page_detail_chat_controller.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart';
+import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:get/get.dart';
+import 'package:uuid/uuid.dart';
+import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 
 class PageDetailChatScreen extends StatefulWidget {
   const PageDetailChatScreen({
@@ -19,6 +23,7 @@ class _PageDetailChatScreenState extends BaseStatefulState<PageDetailChatScreen>
 
   @override
   void initState() {
+    _controller.getData();
     super.initState();
   }
 
@@ -32,11 +37,25 @@ class _PageDetailChatScreenState extends BaseStatefulState<PageDetailChatScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorConstants.appColorBkg,
-      body: Container(
-        color: Colors.red,
-      ),
+      body: Obx(() {
+        return Chat(
+          messages: _controller.messages.value,
+          onSendPressed: _handleSendPressed,
+          showUserAvatars: true,
+          showUserNames: true,
+          user: _controller.userChat.value,
+        );
+      }),
     );
   }
 
-
+  void _handleSendPressed(PartialText message) {
+    final textMessage = types.TextMessage(
+      author: _controller.userChat.value,
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+      id: const Uuid().v4(),
+      text: message.text,
+    );
+    _controller.addMessage(textMessage);
+  }
 }
