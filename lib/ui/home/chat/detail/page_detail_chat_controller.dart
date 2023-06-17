a7742f95a2a3de7d3af2a84877ab084cd4b1297a
HoangGiang09012002 <0306201323@caothang.edu.vn>
2023-06-17 18:51:50 +0700

    implement ux chat detail

--- lib/ui/home/chat/detail/page_detail_chat_controller.dart
+++ lib/ui/home/chat/detail/page_detail_chat_controller.dart
@@ -3,17 +3,23 @@ import 'dart:convert';
 import 'dart:developer';
 
 import 'package:appdiphuot/base/base_controller.dart';
+import 'package:appdiphuot/model/chat.dart';
+import 'package:appdiphuot/model/trip.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart';
 import 'package:get/get.dart';
 
+import '../../../../db/firebase_helper.dart';
 import '../../../../model/user.dart';
+import '../../../../util/log_dog_utils.dart';
 import '../../../../util/shared_preferences_util.dart';
 
 class PageDetailChatController extends BaseController {
-  final CollectionReference _users = FirebaseFirestore.instance.collection('users');
+  final CollectionReference _users = FirebaseHelper.collectionReferenceUser;
+  final CollectionReference _chat = FirebaseHelper.collectionReferenceChat;
 
+  Trip? tripData;
   var userData = UserData().obs;
   var userChat = const User(id: "").obs;
   var messages = <Message>[].obs;
@@ -21,40 +27,51 @@ class PageDetailChatController extends BaseController {
   Future<void> getData() async {
     var currentUid = await SharedPreferencesUtil.getUIDLogin() ?? "";
     _getUserInfo(currentUid);
-    _fetchMessages();
+    _fetchMessages(tripData?.id);
   }
 
   Future<void> _getUserInfo(String uid) async {
     try {
       _users.doc(uid).snapshots().listen((value) {
-        DocumentSnapshot<Map<String, dynamic>>? userMap =
-        value as DocumentSnapshot<Map<String, dynamic>>?;
+        DocumentSnapshot<Map<String, dynamic>>? userMap = value as DocumentSnapshot<Map<String, dynamic>>?;
         if (userMap == null || userMap.data() == null) return;
 
         var user = UserData.fromJson((userMap).data()!);
         userData.value = user;
         userChat.value = User(
           id: user.uid ?? "",
-          lastName: user.name ?? "",
+          firstName: user.name ?? "",
           imageUrl: user.avatar ?? "",
         );
-        log("getUserInfo success: ${user.toString()}");
+        Dog.d("getUserInfo success: ${user.toString()}");
       });
     } catch (e) {
-      log("getUserInfo get user info fail: $e");
+      Dog.e("getUserInfo get user info fail: $e");
     }
   }
 
-    Future<void> _fetchMessages() async {
-    final response = await rootBundle.loadString('assets/files/messages.json');
-    final messages = (jsonDecode(response) as List)
-        .map((e) => Message.fromJson(e as Map<String, dynamic>))
-        .toList();
-    this.messages.value = messages;
+    Future<void> _fetchMessages(String? tripId) async {
+    if (tripId == null) return;
+    _chat.doc(tripId).snapshots().listen((value) {
+      DocumentSnapshot<Map<String, dynamic>>? chatMap = value as DocumentSnapshot<Map<String, dynamic>>?;
+      if (chatMap == null || chatMap.data() == null) return;
+
+      var chatData = Chat.fromJson(chatMap.data()!);
+      messages.value = chatData.messages;
+    });
   }
 
-  void addMessage(Message message) {
+  void addMessage(Message message) async {
     messages.insert(0, message);
+    await _addMessageToFireStore(message);
+  }
+
+  Future<void> _addMessageToFireStore(Message message) async {
+    try {
+      await _chat.doc(tripData?.id).update(Chat(messages).toJson());
+    } catch (e) {
+      Dog.e("_addMessageToFireStore fail: $e");
+    }
   }
 
   void clearOnDispose() {
--- lib/ui/home/chat/page_chat_controller.dart
+++ lib/ui/home/chat/page_chat_controller.dart
@@ -1,15 +1,59 @@
 import 'package:appdiphuot/base/base_controller.dart';
+import 'package:appdiphuot/db/firebase_helper.dart';
+import 'package:appdiphuot/util/shared_preferences_util.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
+
+import '../../../model/trip.dart';
+import '../../../util/log_dog_utils.dart';
 
 class PageChatController extends BaseController {
   var conversationList = <String>[].obs;
 
+  var trips = <Trip>[].obs;
+
   @override
   void onInit() {
     super.onInit();
+    getTrip();
   }
 
   void clearOnDispose() {
     Get.delete<PageChatController>();
+  }
+
+  Future<void> getTrip() async {
+    try {
+      String uid = await SharedPreferencesUtil.getUIDLogin() ?? "";
+      Dog.d("getTrip: userid $uid");
+
+      var routerStream = FirebaseHelper.collectionReferenceRouter.where(
+        FirebaseHelper.listIdMember,
+        arrayContainsAny: [uid]
+      ).snapshots();
+
+      var routerSnapshots = routerStream.map((querySnapshot) => querySnapshot.docs);
+
+      routerSnapshots.listen((routerSnapshots) {
+        var tempTrips = <Trip>[];
+
+        for (var routerSnapshot in routerSnapshots) {
+          Dog.d("getTrip: $routerSnapshot");
+
+          DocumentSnapshot<Map<String, dynamic>>? tripMap = routerSnapshot as DocumentSnapshot<Map<String, dynamic>>?;
+          if (tripMap == null || tripMap.data() == null) return;
+
+          var trip = Trip.fromJson(tripMap.data()!);
+          tempTrips.add(trip);
+        }
+
+        trips.value = tempTrips;
+        Dog.d("getTrip: $trips");
+
+      });
+
+    } catch (e) {
+      Dog.e("getTrip: $e");
+    }
   }
 }
