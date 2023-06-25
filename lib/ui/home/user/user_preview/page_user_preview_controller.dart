dab39cca13007d8971e70428be6deb30b1727483
HoangGiang09012002 <0306201323@caothang.edu.vn>
2023-06-25 09:30:34 +0700

    create page user preview controller

---
+++ lib/ui/home/user/user_preview/page_user_preview_controller.dart
@@ -0,0 +1,113 @@
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:appdiphuot/base/base_controller.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+
+import '../../../../db/firebase_helper.dart';
+import '../../../../model/trip.dart';
+import '../../../../util/shared_preferences_util.dart';
+import '../../../user_singleton_controller.dart';
+
+class PageUserPreviewController extends BaseController {
+  final Rx<File?> _selectedImage = Rx<File?>(null);
+
+  File? get selectedImage => _selectedImage.value;
+
+  var tripParticipatedCount = 0.obs;
+  var totalKm = 0.obs;
+
+  var userData = UserSingletonController.instance.userData;
+  var userChatData = UserSingletonController.instance.userData;
+
+  var trips = <Trip>[].obs;
+  var tripsHost = <Trip>[].obs;
+
+  void clearOnDispose() {
+    Get.delete<PageUserPreviewController>();
+  }
+
+  Future<void> getData() async {
+    getTrip();
+    getTripHost();
+    getTotalTripCount();
+  }
+
+  Future<void> getTrip() async {
+    try {
+      String uid = await SharedPreferencesUtil.getUIDLogin() ?? "";
+      log("getTrip: userid $uid");
+      var routerStream = FirebaseHelper.collectionReferenceRouter
+          .where(FirebaseHelper.listIdMember, arrayContainsAny: [uid])
+          .snapshots();
+
+      var routerSnapshots =
+          routerStream.map((querySnapshot) => querySnapshot.docs);
+
+      routerSnapshots.listen((routerSnapshots) {
+        var tempTrips = <Trip>[];
+
+        for (var routerSnapshot in routerSnapshots) {
+          log("getTrip: $routerSnapshot");
+
+          DocumentSnapshot<Map<String, dynamic>>? tripMap =
+              routerSnapshot as DocumentSnapshot<Map<String, dynamic>>?;
+
+          if (tripMap == null || tripMap.data() == null) return;
+
+          var trip = Trip.fromJson((tripMap).data()!);
+          tempTrips.add(trip);
+        }
+
+        trips.value = tempTrips;
+      });
+    } catch (e) {
+      log("getTrip: $e");
+    }
+  }
+
+  Future<void> getTripHost() async {
+    try {
+      String uid = await SharedPreferencesUtil.getUIDLogin() ?? "";
+      log("getTripHost: userid $uid");
+      var routerStream = FirebaseHelper.collectionReferenceRouter
+          .where(FirebaseHelper.userIdHost, isEqualTo: uid)
+          .snapshots();
+
+      var routerSnapshots =
+          routerStream.map((querySnapshot) => querySnapshot.docs);
+
+      routerSnapshots.listen((routerSnapshots) {
+        var tempTrips = <Trip>[];
+
+        for (var routerSnapshot in routerSnapshots) {
+          log("getTripHost: $routerSnapshot");
+
+          DocumentSnapshot<Map<String, dynamic>>? tripMap =
+              routerSnapshot as DocumentSnapshot<Map<String, dynamic>>?;
+
+          if (tripMap == null || tripMap.data() == null) return;
+
+          var trip = Trip.fromJson((tripMap).data()!);
+          tempTrips.add(trip);
+        }
+
+        tripsHost.value = tempTrips;
+      });
+    } catch (e) {
+      log("getTripHost: $e");
+    }
+  }
+
+  Future<void> getTotalKm() async {
+    // List<Place> fakePlace = fakePlace();
+    // places.value = fakePlace;
+  }
+
+  Future<void> getTotalTripCount() async {
+    // List<Place> fakePlace = fakePlace();
+    // places.value = fakePlace;
+  }
+
+}
