a22865b9d35553a8777c7cc5783c0368a8e23bef
HoangGiang09012002 <0306201323@caothang.edu.vn>
2023-06-25 19:56:07 +0700

    update logic page_user_preview_controller.dart

--- lib/ui/home/user/user_preview/page_user_preview_controller.dart
+++ lib/ui/home/user/user_preview/page_user_preview_controller.dart
@@ -3,11 +3,14 @@ import 'dart:io';
 
 import 'package:appdiphuot/base/base_controller.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_directions/google_maps_directions.dart';
 
 import '../../../../common/const/string_constants.dart';
 import '../../../../db/firebase_helper.dart';
+import '../../../../model/place.dart';
 import '../../../../model/trip.dart';
 import '../../../../model/user.dart';
 import '../../../../util/log_dog_utils.dart';
@@ -20,13 +23,14 @@ class PageUserPreviewController extends BaseController {
   File? get selectedImage => _selectedImage.value;
 
   var tripParticipatedCount = 0.obs;
-  var totalKm = 0.obs;
+  var totalKm = 0.0.obs;
 
   var userData = UserData().obs;
   var userId = "".obs;
 
   var trips = <Trip>[].obs;
   var tripsHost = <Trip>[].obs;
+  var tripsInProgress = <Trip>[].obs;
 
   void clearOnDispose() {
     Get.delete<PageUserPreviewController>();
@@ -36,7 +40,7 @@ class PageUserPreviewController extends BaseController {
     getUserDetail();
     getTrip();
     getTripHost();
-    getTotalTripCount();
+    getTripInProgress();
   }
 
   Future<void> getUserDetail() async {
@@ -121,21 +125,86 @@ class PageUserPreviewController extends BaseController {
     }
   }
 
-  Future<void> getTotalKm() async {
-    // List<Place> fakePlace = fakePlace();
-    // places.value = fakePlace;
-  }
-
-  Future<void> getTotalTripCount() async {
-    // List<Place> fakePlace = fakePlace();
-    // places.value = fakePlace;
-  }
-
   String getUserGender() {
     switch (userData.value.gender) {
       case 0: return StringConstants.male;
       case 1: return StringConstants.female;
       default: return StringConstants.other;
     }
+  }
+
+  Future<void> getTripInProgress() async {
+    try {
+      String uid = await SharedPreferencesUtil.getUIDLogin() ?? "";
+      log("getTripInProgress: userid $uid");
+      var routerStream = FirebaseHelper.collectionReferenceRouter
+          .where(FirebaseHelper.listIdMember, arrayContainsAny: [uid])
+          .where(FirebaseHelper.isComplete, isEqualTo: false)
+          .snapshots();
+
+      var routerSnapshots =
+      routerStream.map((querySnapshot) => querySnapshot.docs);
+
+      routerSnapshots.listen((routerSnapshots) {
+        var tempTrips = <Trip>[];
+
+        for (var routerSnapshot in routerSnapshots) {
+          log("getTripInProgress: $routerSnapshot");
+
+          DocumentSnapshot<Map<String, dynamic>>? tripMap =
+          routerSnapshot as DocumentSnapshot<Map<String, dynamic>>?;
+
+          if (tripMap == null || tripMap.data() == null) return;
+
+          var trip = Trip.fromJson((tripMap).data()!);
+          tempTrips.add(trip);
+        }
+
+        tripsInProgress.value = tempTrips;
+      });
+    } catch (e) {
+      log("getTripInProgress: $e");
+    }
+  }
+
+  Future<void> getTotalKm(List<Trip> trips) async {
+    // try {
+    double totalKmTemp = 0;
+    for (var trip in trips) {
+      if (trip.listPlace == null || trip.listPlace?.isEmpty == true) {
+        double km = await _genDistance(trip.placeStart, trip.placeEnd);
+        totalKmTemp = totalKmTemp + km;
+        Dog.d("getTotalKm km of ${trip.title}: $km");
+      } else {
+        Place? startPlace = trip.placeStart;
+        trip.listPlace?.forEach((place) async {
+          double km = await _genDistance(startPlace, place);
+          totalKmTemp = totalKmTemp + km;
+
+          startPlace = place;
+        });
+
+
+        double km = await _genDistance(startPlace, trip.placeEnd);
+        totalKmTemp = totalKmTemp + km;
+      }
+    }
+    Dog.d("getTotalKm km: $totalKmTemp");
+    totalKm.value = totalKmTemp;
+  }
+
+  Future<double> _genDistance(Place? placeA, Place? placeB) async {
+    Dog.d("_genDistance: - A: ${placeA?.lat} - ${placeA?.long},  B: ${placeB?.lat} - ${placeB?.long}");
+    if (placeA == null || placeB == null) return 0;
+    DistanceValue distanceBetween = await distance(
+      placeA.lat ?? 0,
+      placeA.long ?? 0,
+      placeB.lat ?? 0,
+      placeB.long ?? 0,
+    );
+    int meters = distanceBetween.meters;
+    String textInKmOrMeters = distanceBetween.text;
+    debugPrint(">>>_genDistance meters $meters, textInKmOrMeters $textInKmOrMeters");
+    return meters / 1000;
   }
 }
