d93071c1f06156369a3afe62cf27cde2099a4fef
ChauTriTin <freuss47@gmail.com>
2023-06-26 22:01:45 +0700

    update

--- lib/ui/home/router/map/map_controller.dart
+++ lib/ui/home/router/map/map_controller.dart
@@ -257,32 +257,32 @@ class MapController extends BaseController {
     }
   }
 
-  Future<void> _genDistance() async {
-    DistanceValue distanceBetween = await distance(
-      9.2460524,
-      1.2144565,
-      6.1271617,
-      1.2345417,
-    );
-    int meters = distanceBetween.meters;
-    String textInKmOrMeters = distanceBetween.text;
-    debugPrint(
-        ">>>_genDistance meters $meters, textInKmOrMeters $textInKmOrMeters");
-  }
-
-  Future<void> _genDuration() async {
-    DurationValue durationBetween = await duration(
-      9.2460524,
-      1.2144565,
-      6.1271617,
-      1.2345417,
-    );
-
-    int seconds = durationBetween.seconds;
-    String durationInMinutesOrHours = durationBetween.text;
-    debugPrint(
-        ">>>_genDuration seconds $seconds, durationInMinutesOrHours $durationInMinutesOrHours");
-  }
+  // Future<void> _genDistance() async {
+  //   DistanceValue distanceBetween = await distance(
+  //     9.2460524,
+  //     1.2144565,
+  //     6.1271617,
+  //     1.2345417,
+  //   );
+  //   int meters = distanceBetween.meters;
+  //   String textInKmOrMeters = distanceBetween.text;
+  //   debugPrint(
+  //       ">>>_genDistance meters $meters, textInKmOrMeters $textInKmOrMeters");
+  // }
+  //
+  // Future<void> _genDuration() async {
+  //   DurationValue durationBetween = await duration(
+  //     9.2460524,
+  //     1.2144565,
+  //     6.1271617,
+  //     1.2345417,
+  //   );
+  //
+  //   int seconds = durationBetween.seconds;
+  //   String durationInMinutesOrHours = durationBetween.text;
+  //   debugPrint(
+  //       ">>>_genDuration seconds $seconds, durationInMinutesOrHours $durationInMinutesOrHours");
+  // }
 
   Future<void> postFCM(
     String body,
