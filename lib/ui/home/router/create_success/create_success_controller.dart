2aceb088150d3965a7e895f11015c16bd3c86f76
ChauTriTin <freuss47@gmail.com>
2023-06-26 22:23:21 +0700

    update

--- lib/ui/home/router/create_success/create_success_controller.dart
+++ lib/ui/home/router/create_success/create_success_controller.dart
@@ -1,8 +1,14 @@
 import 'package:appdiphuot/base/base_controller.dart';
+import 'package:appdiphuot/db/firebase_helper.dart';
+import 'package:appdiphuot/model/trip.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 class CreateSuccessController extends BaseController {
   var isDoneCountdown = false.obs;
+  var trip = Trip().obs;
 
   void clearOnDispose() {
     Get.delete<CreateSuccessController>();
@@ -10,5 +16,48 @@ class CreateSuccessController extends BaseController {
 
   void setDoneCountdown(bool value) {
     isDoneCountdown.value = value;
+  }
+
+  Future<void> getRouter(String id) async {
+    try {
+      FirebaseHelper.collectionReferenceRouter
+          .doc(id)
+          .snapshots()
+          .listen((value) {
+        DocumentSnapshot<Map<String, dynamic>>? map =
+            value as DocumentSnapshot<Map<String, dynamic>>?;
+        if (map == null || map.data() == null) return;
+
+        var trip = Trip.fromJson((map).data()!);
+        this.trip.value = trip;
+        debugPrint("getRouter success: ${trip.toString()}");
+
+        //gen list router
+        var pStart = this.trip.value.placeStart;
+        var pEnd = this.trip.value.placeEnd;
+        var list = this.trip.value.listPlace;
+        // if (pStart != null && pEnd != null && list != null) {
+        //   _initRouter(pStart, pEnd, list);
+        // }
+
+        //gen list member
+        // _genListMember(this.trip.value.listIdMember ?? List.empty());
+      });
+    } catch (e) {
+      debugPrint("getRouter get user info fail: $e");
+    }
+  }
+
+  DateTime getDateTimeEnd() {
+    try {
+      var dateTimeEnd = trip.value.timeEnd ?? "";
+      debugPrint("getDateTimeEnd dateTimeEnd $dateTimeEnd");
+      DateTime tempDate = DateFormat("dd/MM/yyyy HH:mm").parse(dateTimeEnd);
+      debugPrint("getDateTimeEnd tempDate $tempDate");
+      return tempDate;
+    } catch (e) {
+      debugPrint("getDateTimeEnd error: $e");
+      return DateTime.now().add(const Duration(days: 7));
+    }
   }
 }
